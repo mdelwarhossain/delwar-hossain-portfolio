a8ccf6783f5bb0d33ff550a15205b38e2c24f307
mddelwarhossain <dhsaeed88@gmail.com>
2022-12-10 17:15:52 +0100

    home about section edited

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import portfolio from '../../assets/portfolio2.jpg'
 
 const About = () => {
     return (
-        <div id='about' className="hero min-h-screen lg:w-2/3 lg:mx-auto my-5">
-            <div className="hero-content flex-col lg:flex-row">
-                <img src="https://placeimg.com/260/400/arch" className="w-1/2 rounded-lg shadow-2xl" alt='profile' />
-                <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+        <div>
+            <h1 className='text-5xl font-bold text-center'>My Introduction</h1>
+            <div id='about' className="hero min-h-screen lg:w-2/3 lg:mx-auto my-5">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={portfolio} className="w-1/2 rounded-lg shadow-2xl" alt='profile' />
+                    <div>
+                        <h1 className="text-3xl font-bold">A passionate web developer</h1>
+                        <p className="py-6">I am a fresher but confident in frontend engineering. I am comfortable in using modern frontend web technologies to develop a responsive professional website. I got effective communication skills and adaptive in a cosmopolitan and multifunctional team. I am a quick learner and always love to explore boyond my comfort zone.</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import portfolio from '../../assets/portfolio1.jpg'
 
 const Home = () => {
     return (
         <div className="hero min-h-screen lg:w-2/3 lg:mx-auto my-2">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" alt='profile'/>
+                <img src={portfolio} className="w-1/2 max-h-screen rounded-lg shadow-2xl" alt='profile'/>
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-accent">Download Resume</button>
+                    <h1 className="text-5xl font-bold"><span className='text-accent'>Hi!!!</span><br /> I'm Md. Delwar Hossain</h1>
+                    <p className="py-6">Junior Frontend Engineer. Develope responsive website using modern web technologies.</p>
+                    <a className='btn btn-accent' href="https://drive.google.com/uc?export=download&id=1z5Zhk6beBiG-JGoVFRRic1q1TJp-dSCJ">Download Resume</a>
                 </div>
             </div>
         </div>
