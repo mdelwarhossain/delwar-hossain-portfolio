e92d9547a3e16c556fed076dc52792a5f8869fbf
mddelwarhossain <dhsaeed88@gmail.com>
2022-12-11 16:26:01 +0100

    improved

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div id='home' className="hero min-h-screen lg:w-2/3 lg:mx-auto my-2">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={portfolio} className="w-1/2 max-h-screen rounded-lg shadow-2xl" alt='profile'/>
+                <img src={portfolio} className="md:w-1/2 w-3/4 max-h-screen rounded-lg shadow-2xl" alt='profile'/>
                 <div>
                     <h1 className="text-5xl font-bold"><span className='text-accent'>Hi!!!</span><br /> I'm Md. Delwar Hossain</h1>
                     <p className="py-6">Junior Frontend Engineer. Develope responsive website using modern web technologies.</p>
